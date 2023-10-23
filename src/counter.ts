bb7af1ba9980499e14ee99aefab63ad66de4084f
allen <eepson123@gmail.com>
2023-10-23 23:04:45 +0800

    feat(css): add the base html + css

--- src/counter.ts
+++ src/counter.ts
@@ -1,9 +1,33 @@
-export function setupCounter(element: HTMLButtonElement) {
-  let counter = 0
-  const setCounter = (count: number) => {
-    counter = count
-    element.innerHTML = `count is ${counter}`
+export function mount(text: string) {
+  const inputArea = ` <div tabindex="0" class="flex bg-white b-2 b-solid b-rounded b-[#dadce0] text-black mx-[5px] pr-[14px] pl-[10px] pt-[10px] flex-wrap hover:drop-shadow hover:shadow-inset jx-input">
+<div class="j-history w-[22px] h-[22px] text-gray">
+  <svg  fill="rgb(112, 117, 122)" focusable="false" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M13 3a9 9 0 0 0-9 9H1l3.89 3.89.07.14L9 12H6c0-3.87 3.13-7 7-7s7 3.13 7 7-3.13 7-7 7c-1.93 0-3.68-.79-4.94-2.06l-1.42 1.42A8.954 8.954 0 0 0 13 21a9 9 0 0 0 0-18zm-1 5v5l4.28 2.54.72-1.21-3.5-2.08V8H12z"></path></svg>
+</div>
+<div class="jx-pre-movement flex-1 text-right text-[#70757A]">8+8=</div>   
+<div class="text-[30px] text-truncate w-full text-right">16</div> 
+</div>`
+
+  if (text === 'inputArea') {
+    return inputArea
   }
-  element.addEventListener('click', () => setCounter(counter + 1))
-  setCounter(0)
+
+  return ''
+}
+
+export function getInputFocus(
+  element: HTMLDivElement,
+  focusElement: HTMLDivElement
+) {
+  element.addEventListener('click', (e) => {
+    const currentClassList = focusElement.classList
+    currentClassList.toggle('b-[#dadce0]')
+    currentClassList.toggle('b-[#4285f4]')
+  })
+  // let counter = 0
+  // const setCounter = (count: number) => {
+  //   counter = count
+  //   element.innerHTML = `count is ${counter}`
+  // }
+  // element.addEventListener('click', () => setCounter(counter + 1))
+  // setCounter(0)
 }
--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,306 @@
 import '@unocss/reset/normalize.css'
 import './style.css'
 import 'virtual:uno.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import { mount, getInputFocus } from './counter.ts'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
+  <div class="text-center b-1 b-white b-solid p-3 w-[400px] md:w-[600px] lg:w-[800px] ">
+    <h1>Google Calculator</h1> 
+    <!-- Input Area ---!>   
+    <div>
+      ${mount('inputArea')}
     </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-    <p class='bg-black text-center text-blue font-900'>Test unocss<p>
+    <div class="mx-[5px]">
+      <div class='grid grid-cols-7 grid-rows-4 mt-2 text-[13px] gap-3'> 
+        <!-- TOP ---!>
+        <div class="col-span-2 flex input-btn">
+          <div role="button" tabindex="0" aria-label="切換弧度和角度" class='flex-1'>Rad</div>|
+          <div role="button" tabindex="0" aria-label="切換弧度和角度" class='flex-1'>Deg</div>
+        </div>
+        <div class="input-btn" role="button" tabindex="0" aria-label="階乘">x!</div>
+        <div class="input-btn" role="button" tabindex="0" aria-label="左括號">(</div>
+        <div class="input-btn" role="button" tabindex="0" aria-label="右括號">(</div>
+        <div class="input-btn" role="button" tabindex="0" aria-label="百分比">%</div>
+        <div class="input-btn" role="button" tabindex="0" aria-label="清除項目">CE</div>
+        <!-- MIDDLE 1 ---!>
+        <div class="input-btn" role="button" tabindex="0" aria-label="倒數">Inv</div>
+        <div class="input-btn">
+          <div role="button" tabindex="0" aria-label="正弦" >sin</div>
+          <div style="display:none" role="button" tabindex="0" aria-label="反正弦" > sin<sup>−1</sup></div>
+        </div>
+        <div class="input-btn">
+          <div role="button" tabindex="0" aria-label="自然對數" > ln </div>
+          <div style="display:none"  role="button" tabindex="0" aria-label="E 的 X 次方" > e<sup>x</sup></div>
+        </div>
+        <div class="input-btn input-btn--light" role="button" tabindex="0">7</div>
+        <div class="input-btn input-btn--light" role="button" tabindex="0">8</div>
+        <div class="input-btn input-btn--light" role="button" tabindex="0">9</div>
+        <div class="input-btn font-size-[18px]" role="button" tabindex="0">÷</div>
+        <!-- MIDDLE 2 ---!>
+        <div class="input-btn" role="button" tabindex="0" aria-label="圓周率">π</div>
+        <div class="input-btn">
+          <div role="button" tabindex="0" aria-label="餘弦" >cos</div>
+          <div style="display:none"  role="button" tabindex="0" aria-label="反餘弦"> cos<sup>−1</sup></div>
+        </div>
+        <div class="input-btn">
+          <div  role="button" tabindex="0" aria-label="對數" >log</div>
+          <div style="display:none"  role="button" tabindex="0" aria-label="十的 X 次方" > 10<sup>x</sup></div>
+        </div>
+        <div class="input-btn input-btn--light" role="button" tabindex="0">4</div>
+        <div class="input-btn input-btn--light" role="button" tabindex="0">5</div>
+        <div class="input-btn input-btn--light" role="button" tabindex="0">6</div>
+        <div class="input-btn font-size-[18px]" role="button" tabindex="0">x</div>
+        <!-- MIDDLE 3 ---!>
+        <div class="input-btn">
+          <div role="button" tabindex="0" aria-label="自然對數" > ln </div>
+          <div style="display:none"  role="button" tabindex="0" aria-label="E 的 X 次方" > e<sup>x</sup></div>
+        </div>
+        <div class="input-btn">
+          <div role="button" tabindex="0" aria-label="正切" >tan</div>
+          <div style="display:none"  role="button" tabindex="0" aria-label="反正切" > tan<sup>−1</sup></div>
+        </div>
+        <div class="input-btn">
+          <div role="button" tabindex="0" aria-label="平方根" >√ </div>
+          <div style="display:none"  role="button" tabindex="0" aria-label="平方" > x<sup>2</sup></div>
+        </div>
+        <div class="input-btn input-btn--light" role="button" tabindex="0">1</div>
+        <div class="input-btn input-btn--light" role="button" tabindex="0">2</div>
+        <div class="input-btn input-btn--light" role="button" tabindex="0">3</div>
+        <div class="input-btn font-size-[18px]" role="button" tabindex="0">-</div>
+        <!-- FOOTER ---!>
+        <div class="input-btn">
+          <div  role="button" tabindex="0" aria-label="答案" >Ans</div>
+          <div  style="display:none"  role="button" tabindex="0" aria-label="隨機">Rnd</div>
+        </div>
+        <div class="input-btn" role="button" tabindex="0" aria-label="指數">EXP</div>
+        <div class="input-btn line-height-[26px]">
+          <div  role="button" tabindex="0" aria-label="X 的 Y 次方" >x<sup>y</sup></div>
+          <div style="display:none"  role="button" tabindex="0" aria-label="X 的 Y 次方根" > <sup>y</sup>√x</div>
+        </div>
+        <div class="input-btn input-btn--light" role="button" tabindex="0">0</div>
+        <div class="input-btn input-btn--light" role="button" tabindex="0">.</div>
+        <div class="input-btn input-btn--blue font-size-[18px]" role="button" tabindex="0">=</div>
+        <div class="input-btn font-size-[18px]" role="button" tabindex="0">+</div>
+      </div>
+    </div> 
   </div>
 `
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+getInputFocus(
+  document.querySelector<HTMLDivElement>('.jx-input')!,
+  document.querySelector<HTMLDivElement>('.jx-input')!
+)
+
+/* 先留者看 methods
+ <table class="HOoTuc">
+      <tbody>
+        <tr>
+          <td class="" colspan="2">
+            <div class="">
+              <div role="button" tabindex="0" aria-label="切換弧度和角度" >
+                <div class="Tqvz7b">Rad</div>
+                <div class="Lm2Tt"></div>
+                <div class="SwDSDf">Deg</div>
+              </div>
+            </div>
+          </td>
+          <td>
+            <div class="">
+              <div role="button" tabindex="0" aria-label="階乘" >x!</div>
+            </div>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <div class="">
+              <div  role="button" tabindex="0" aria-label="倒數" >Inv</div>
+            </div>
+          </td>
+          <td>
+            <div class="">
+              <div  role="button" tabindex="0" aria-label="正弦" >sin</div>
+              <div style="display:none"  role="button" tabindex="0" aria-label="反正弦" > sin<sup>−1</sup></div>
+            </div>
+          </td>
+          <td>
+            <div class="">
+              <div role="button" tabindex="0" aria-label="自然對數" > ln </div>
+              <div style="display:none"  role="button" tabindex="0" aria-label="E 的 X 次方" > e<sup>x</sup></div>
+            </div>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <div class="">
+              <div role="button" tabindex="0" aria-label="圓周率" >π</div>
+            </div>
+          </td>
+          <td>
+            <div class="">
+              <div  role="button" tabindex="0" aria-label="餘弦" >cos</div>
+              <div style="display:none"  role="button" tabindex="0" aria-label="反餘弦" > cos<sup>−1</sup></div>
+            </div>
+          </td>
+          <td>
+            <div class="">
+              <div  role="button" tabindex="0" aria-label="對數" >log</div>
+              <div style="display:none"  role="button" tabindex="0" aria-label="十的 X 次方" > 10<sup>x</sup></div>
+            </div>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <div class="">
+              <div role="button" tabindex="0" aria-label="尤拉數" >e</div>
+            </div>
+          </td>
+          <td>
+            <div class="">
+              <div  role="button" tabindex="0" aria-label="正切" >tan</div>
+              <div tyle="display:none"  role="button" tabindex="0" aria-label="反正切" > tan<sup>−1</sup></div>
+            </div>
+          </td>
+          <td>
+            <div class="">
+              <div role="button" tabindex="0" aria-label="平方根" >√ </div>
+              <div style="display:none"  role="button" tabindex="0" aria-label="平方" > x<sup>2</sup></div>
+            </div>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <div class="">
+              <div  role="button" tabindex="0" aria-label="答案" >Ans</div>
+              <div tyle="display:none"  role="button" tabindex="0" aria-label="隨機" > Rnd</div>
+            </div>
+          </td>
+          <td>
+            <div class="">
+              <div role="button" tabindex="0" aria-label="指數" >EXP</div>
+            </div>
+          </td>
+          <td>
+            <div class="">
+              <div  role="button" tabindex="0" aria-label="X 的 Y 次方" >x<sup>y</sup></div>
+              <div style="display:none"  role="button" tabindex="0" aria-label="X 的 Y 次方根" > <sup>y</sup>√x</div>
+            </div>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+    <table class="ElumCf">
+      <tbody>
+        <tr>
+          <td>
+            <div class="">
+              <div role="button" tabindex="0" aria-label="左括號" >(</div>
+            </div>
+          </td>
+          <td>
+            <div class="">
+              <div ole="button" tabindex="0" aria-label="右括號" >)</div>
+            </div>
+          </td>
+          <td>
+            <div class="">
+              <div ole="button" tabindex="0" aria-label="百分比" >%</div>
+            </div>
+          </td>
+          <td>
+            <div class="">
+              <div ousedown:JHz26d" role="button" tabindex="0" aria-label="全部清除"  style="display: none;">AC</div>
+              <div tyle="" mousedown:JHz26d" role="button" tabindex="0" aria-label="清除項目" >CE</div>
+            </div>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <div class="">
+              <div role="button" tabindex="0" >7</div>
+            </div>
+          </td>
+          <td>
+            <div class="">
+              <div role="button" tabindex="0" >8</div>
+            </div>
+          </td>
+          <td>
+            <div class="">
+              <div ole="button" tabindex="0" >9</div>
+            </div>
+          </td>
+          <td>
+            <div class="PaQdxb mF5fo">
+              <div role="button" tabindex="0" aria-label="除" >÷</div>
+            </div>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <div class="">
+              <div ole="button" tabindex="0" >4</div>
+            </div>
+          </td>
+          <td>
+            <div class="">
+              <div ole="button" tabindex="0" >5</div>
+            </div>
+          </td>
+          <td>
+            <div class="">
+              <div role="button" tabindex="0" >6</div>
+            </div>
+          </td>
+          <td>
+            <div class="PaQdxb mF5fo">
+              <div role="button" tabindex="0" aria-label="乘" >×</div>
+            </div>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <div class="">
+              <div ole="button" tabindex="0" >1</div>
+            </div>
+          </td>
+          <td>
+            <div class="">
+              <div role="button" tabindex="0" >2</div>
+            </div>
+          </td>
+          <td>
+            <div class="">
+              <div ole="button" tabindex="0" >3</div>
+            </div>
+          </td>
+          <td>
+            <div class="PaQdxb mF5fo">
+              <div role="button" tabindex="0" aria-label="減" >−</div>
+            </div>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <div class="">
+              <div role="button" tabindex="0" >0</div>
+            </div>
+          </td>
+          <td>
+            <div class="PaQdxb mF5fo G7BHUb">
+              <div role="button" tabindex="0" aria-label="點" >.</div>
+            </div>
+          </td>
+          <td>
+            <div class="PaQdxb mF5fo G7BHUb">
+              <div role="button" tabindex="0" aria-label="等於" >=</div>
+            </div>
+          </td>
+          <td>
+            <div class="PaQdxb mF5fo">
+              <div role="button" tabindex="0" aria-label="加" >+</div>
+            </div>
+          </td>
+        </tr>
+        </tbody>
+      </table>
+*/
